8ee44cccfcfd8dfa38ba361031b1599c9c1e5f14
ghkdtjfxkd <syt1128@gmail.com>
2025-09-03 10:49:08 +0900

    feat: 문자열 입력 안내 메시지 출력

---
+++ src/main/java/calculator/IO/UserInput.java
@@ -0,0 +1,10 @@
+package calculator.IO;
+
+public class UserInput {
+
+    private static final String INPUT_INFO_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+
+    private static void announceInfoMessage() {
+        System.out.println(INPUT_INFO_MESSAGE);
+    }
+}
